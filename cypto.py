e8d55769b751696fd997415fcba5bf11da575fca
BlackcatRs <45454897+BlackcatRs@users.noreply.github.com>
2020-02-17 20:19:15 +0100

    Create cypto.py

---
+++ cypto.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python3
+
+from OutilsCrypto import *
+
+txt="BONJOUR"
+clef=3
+
+
+
+def Ecesar(txt, clef, paq=1) :
+    list = []
+    var=paquet(txt, 1)
+    for valeur in var.values():
+        valeur += clef
+        valeur %= mod2base(paq)
+        list.append(xedoc(valeur))
+
+    txtCodee = ""
+    for value in list:
+        txtCodee += value
+
+    return txtCodee
+
+
+
+print("Voila votre text <<", txt,">> codee par paquet de 1 : ", Ecesar(txt, clef))
