b2be2502e76a3935d2a843d6b820d1307f68563a
vithurshan <you@example.com>
2020-02-20 12:51:49 -0800

    regrouper les lettres de paquet de x ajouter la cle a cette paquet regroupé

--- crypto.py
+++ crypto.py
@@ -2,17 +2,40 @@
 
 from OutilsCrypto import *
 
-txt="bonjour"
+txt="bonjourz"
 clef=3
 
-'''
-def Ecesar(txt, clef, paq=1) :
-    list = []
+
+def Ecesar(txt, clef, paq=2) :
+    list = [] #pour stocker les resultat a la fin
     var=paquet(txt, 1) #transformer le txt en dic
+
+    list1=[]
     for valeur in var.values():
-        valeur += clef #ajouter la cle
-        valeur %= mod2base(paq) #modulo 26
-        list.append(xedoc(valeur)) #ajoute les lettre converti des numero
+        list1.append(valeur)
+
+    list2=[]
+    b=0
+    res=int((len(list1)-(len(list1)%paq))/paq)
+    print(res)
+    for values in range(res):
+        i=0
+        a=''
+        while i < paq:
+            a+=str(list1[b])
+            i+=1 #comteur
+            b+=1 #sauter lettre par lettre
+        list2.append(a)
+    print(list2)
+
+    for valeur in list2:
+        #print(valeur)
+        trans = int(valeur)
+        trans += clef #ajouter la cle
+        trans %= mod2base(paq) #modulo 26
+        print(trans)
+        #separer les lettre 2 par deux puis les transformer en lettre
+        list.append(xedoc(trans)) #ajoute les lettre converti des numero
 
     txtCodee = ""
     for value in list:
@@ -23,45 +46,4 @@ def Ecesar(txt, clef, paq=1) :
 
 
 print("Voila votre text <<", txt,">> codee par paquet de 1 : ", Ecesar(txt, clef))
-
-'''
-
-
-list=[1, 2, 5, 6, 7, 1]
-list1=[]
-paq = 3
-
-'''#1er 2 chiffre
-b=0
-i=0
-a=''
-
-while i < paq:
-    a+=str(list[b])
-    i+=1
-    b+=1
-
-list1.append(a)
-
- #2eme 2 chifrre
-i=0
-c=''
-while i < paq:
-    c+=str(list[b])
-    i+=1
-    b+=1
-
-list1.append(c)
-
-'''
-b=0
-for values in range(paq):
-    i=0
-    a=''
-    while i < paq:
-        a+=str(list[b])
-        i+=1
-        b+=1
-    list1.append(a)
-
-print(list1)
+print(paquet(txt, 1).values())
