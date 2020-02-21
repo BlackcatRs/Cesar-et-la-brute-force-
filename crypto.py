26a7069156fa2ef6253ee9abd1ee80fee6d6cab1
vithurshan <you@example.com>
2020-02-21 11:22:56 -0800

    final

--- crypto.py
+++ crypto.py
@@ -2,32 +2,34 @@
 
 from OutilsCrypto import *
 
-txt="bonjour"
-clef=3
+#txt="bonjour"
+#clef=3
+txt = input("Entrez votre texte ici : ")
+paq = int(input("Entrez nobre de paquet : "))
+clef = int(input("Entrez votre clef : "))
 
+def Ecesar(txt, clef) :
 
-def Ecesar(txt, clef, paq=2) :
-    list = [] #pour stocker les resultat a la fin
-    list1 = []
+    dic=paquet(txt, paq) #transformer le texte en chiffre par paquet de paq et stocker dans un dictionnaire
 
-    dic=paquet(txt, paq) #transformer le txt en dic
-
-    #transformer le dic en list
+    #transformer le dictionnaire en list
+    list = []
     for value in dic.values():
         list.append(value)
-    print(list)
+    print("votre texte <<", txt, ">> en chiffre", list,  "par paquet de", paq)
 
-    #ajouter la clef
+    #ajouter la clef a la liste
     compteur=0
     for value in list:
         list[compteur]+=clef
         compteur+=1
 
-    #transformer les int dans la list en str
+    #transformer les int de la liste en str et stocker dans une autre liste "liste1"
+    list1 = []
     for value in list:
         list1.append(str(value))
 
-    #ajouter les 0 pour creer les paquet
+    #ajouter les 0 devant un nombre pour creer les paquet
     compteur=0
     for value in list1:
         if len(value) < paq*2:
@@ -35,14 +37,28 @@ def Ecesar(txt, clef, paq=2) :
         compteur=+1
     print(list1)
 
+    #separer les chiffre 2 par 2
+    list2 = []
     for value in list1:
-        print(value[0:2])
-        for value in range(2):
-            print(xedoc(int(value[0:2])))
+        compteur1 = 0
+        compteurL = 0
 
+        while compteur1 < paq:
+            a=''
+            compteur = 0
+            while compteur < 2:
+                a+=value[compteurL]
+                compteur+=1
+                compteurL+=1
+            list2.append(int(a))
+            compteur1+=1
+    print(list2)
 
-    return list1
+    #transformer les chiffre regroupe 2 par 2 en lettre
+    motcrypte = ''
+    for element in list2:
+        motcrypte+=xedoc(element)
 
+    return motcrypte
 
-#print("Voila votre text <<", txt,">> codee par paquet de 1 : ", Ecesar(txt, clef))
-Ecesar(txt,clef)
+print("Voila votre texte codee", Ecesar(txt,clef))
